ab827a0102586d0c254457d1ac4acadfbf088155
antonm7 <40800180+antonm7@users.noreply.github.com>
2019-08-17 21:15:00 +0300

    database change

--- database.js
+++ database.js
@@ -13,8 +13,8 @@ class Database {
         //database settings
         this.cfg = {
             host: '127.0.0.1',
-            port: 27017,
-            dbname: 'default-database',
+            port: 56405,
+            dbname: 'gameDatabase',
         };
     }
 

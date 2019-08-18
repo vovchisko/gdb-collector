34b09900281956a1bca1385c7313ec6ff0f46581
antonm7 <40800180+antonm7@users.noreply.github.com>
2019-08-18 18:59:09 +0300

    chnage db

--- database.js
+++ database.js
@@ -13,7 +13,7 @@ class Database {
         //database settings
         this.cfg = {
             host: '167.71.44.65',
-            port: 27017,
+            port: 8080,
             dbname: 'gameDatabase',
         };
     }
